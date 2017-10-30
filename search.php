23f371531e164f836fc40e34e6ce6785d15e6de4
Ec2 <rkarimian@eng.ucsd.edu>
2017-10-30 21:18:39 +0000

    fixed redirect problem

--- search.php
+++ search.php
@@ -1,7 +1,6 @@
 <?php
    include_once 'header.php';
 ?>
-
 <div class="container-fluid">
 		<form action="" method="POST">
         <div class="input-group">
@@ -13,48 +12,46 @@
           </div>
         </div>
 	  </form>
-	<?php
-	if (isset($_POST['submit'])){
-		include_once 'includes/dbh.inc.php';
-		$searchTerm = mysqli_real_escape_string($conn, $_POST['search']);
-		$sql = "SELECT * FROM `items` WHERE type = '$searchTerm' LIMIT 5";
-		$result = mysqli_query($conn, $sql);
-		$resultCheck = mysqli_num_rows($result);
-    $imgPerRow=4;
-    if ($resultCheck>0){
-      $i=1;
-		  while($row=mysqli_fetch_array($result)){
-        if ($i==1){
-          echo '<div class="row">';
-        }
-        echo '<div class="col-md-'.round(12/$imgPerRow).'"><div class="thumbnail">';
-        $name  =$row['name'];
-			  $url=$row['url'];
-			  $filename=$row['filepath'];
-        echo '<a  href="'.$url.'"><img class="square-image" src="pictures/'.$filename.'"></a>
-        <p><button type="button" class="btn btn-success btn-sm">
+	  <?php
+		if (isset($_POST['submit'])){
+			include_once 'includes/dbh.inc.php';
+			$searchTerm = mysqli_real_escape_string($conn, $_POST['search']);
+			$sql = "SELECT * FROM `items` WHERE type = '$searchTerm' LIMIT 5";
+			$result = mysqli_query($conn, $sql);
+			$resultCheck = mysqli_num_rows($result);
+    		$imgPerRow=4;
+    		if ($resultCheck>0){
+      			$i=1;
+		  		while($row=mysqli_fetch_array($result)){
+        			if ($i==1){
+          				echo '<div class="row">';
+        			}
+        			echo '<div class="col-md-'.round(12/$imgPerRow).'"><div class="thumbnail">';
+        			$name  =$row['name'];
+			  		$url=$row['url'];
+			  		$filename=$row['filepath'];
+       				 echo '<a  href="'.$url.'"><img class="square-image" src="pictures/'.$filename.'"></a>
+       				 <p><button type="button" class="btn btn-success btn-sm">
           <span class="glyphicon glyphicon-thumbs-up"></span> Like
         </button></p><p><button type="button" class="btn btn-danger btn-sm">
           <span class="glyphicon glyphicon-thumbs-down"></span> Like
         </button></p>';
-        echo '</div></div>';
-        $i=$i+1;
-        if ($i==($imgPerRow+1)){
-          echo '</div>';
-          $i=$i%$imgPerRow;
-        }
-		  }
+        			echo '</div></div>';
+        			$i=$i+1;
+        			if ($i==($imgPerRow+1)){
+          				echo '</div>';
+          				$i=$i%$imgPerRow;
+        			}
+		  		}
 
-   }else{
-      echo 'No matching images';
-    }
+   			}else{
+      			echo 'No matching images';
+    		}
+		}
+	   ?>
+	
 
-	}else{
-		header("Location: search.php");
-		exit();
-	}
 
-?>
 </div>
 <?php
    include_once 'footer.php';
--- test.php
+++ test.php
@@ -10,12 +10,9 @@ if (isset($_POST['submit'])){
   #$res=system('add.py',$retval);
   #echo $r;
   #$i =`python add.py`;
-  $i=shell_exec('/Users/dreamer/anaconda/bin/python -m scripts.label_image --graph=tf_files/retrained_graph.pb  --image=Baxton-Studio-Vino-Modern-Upholstered-Full-size-Bed-Headboard-P15358380.jpg 2>&1|grep -v ');
+  $i=shell_exec('python -m scripts.label_image --graph=tf_files/retrained_graph.pb  --image=Baxton-Studio-Vino-Modern-Upholstered-Full-size-Bed-Headboard-P15358380.jpg 2>&1');
   #$i=shell_exec('/Users/dreamer/anaconda/bin/python -V 2>&1');
   echo $i;
-}else{
-  header("Location: test.php");
-  exit();
 }
  ?>
 <?php
