c907cc5893d3c4c3afa70d837f9135a26cc09758
Ec2 <rkarimian@eng.ucsd.edu>
2017-11-03 20:47:32 +0000

    material model, upload

--- test.php
+++ test.php
@@ -1,20 +1,42 @@
 <?php
    include_once 'header.php';
 ?>
-<form action="test.php" method="Post">
-  <input type="text"><br>
-  <button type="submit" name="submit">Search</button>
-</form>
 <?php
-if (isset($_POST['submit'])){
-  #$res=system('add.py',$retval);
-  #echo $r;
-  #$i =`python add.py`;
-  $i=shell_exec('python -m scripts.label_image --graph=tf_files/retrained_graph.pb  --image=Baxton-Studio-Vino-Modern-Upholstered-Full-size-Bed-Headboard-P15358380.jpg 2>&1');
-  #$i=shell_exec('/Users/dreamer/anaconda/bin/python -V 2>&1');
-  echo $i;
+	$dir='pictures';
+	#$num=1;
+    $files = glob($dir . '/*.*');
+    #echo $_GLOBALS['key'];
+    
+  ?>
+  
+<?php
+if (isset($_POST['material'])){
+   $file=$files[$_POST['key']];
+   echo '<img width="200px"  src="'.$file.'"><br>';
+   echo '<form action="test.php" method="Post"><button type="submit" name="next">Next</button></form>';
+   $statement='python -m scripts.label_image --graph=tf_files/retrained_graph.pb  --image='.$file.' 2>&1';
+   $i=shell_exec($statement);
+   #echo $statement;
+   $pattern1 = "/(.+\.jpg)/";
+
+   $new=preg_replace($pattern1, "", $i);
+   $pattern2="/([a-zA-Z]+) ([0-9].[0-9]+.e?-?[0-9]?[0-9]?)/";
+   preg_match_all($pattern2, $new, $matches);
+   $c=count($matches[0]);
+   for ($n=0;$n<$c;$n++){
+	   echo "<li>".$matches[1][$n]." ".$matches[2][$n]."</li>";
+   }
+}else{
+	$key = array_rand($files);
+    $file=$files[$key];
+    echo '<img width="200px"  src="'.$file.'"><br>';
+    echo '<form action="test.php" method="Post">
+    <input type="hidden" name="key" value="'.$key.'" />
+    <button type="submit" name="material">Material</button>
+    <button type="submit" name="next">Next</button></form>';
 }
  ?>
+  
 <?php
  include_once 'footer.php';
 ?>
